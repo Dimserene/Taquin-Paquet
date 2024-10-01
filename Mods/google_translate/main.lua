2bae4eaee55aff6fe8de4593bdafcaa6c289ef47
Dimserene <raul0520@google.com>
2024-10-01 23:13:08 +0800

    updates

---
+++ Mods/google_translate/main.lua
@@ -0,0 +1,10 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Google Translated
+--- MOD_ID: googletranslated
+--- MOD_AUTHOR: [Phrog]
+--- MOD_DESCRIPTION: Google Translated all contexts
+--- BADGE_COLOUR: 000000
+--- DISPLAY_NAME: Google Translated 
+--- PREFIX: GT
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-0812d]
